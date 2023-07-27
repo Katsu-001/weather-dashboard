8aea484cee5145b867344938b1aeb3ffd9629a0e
Katsuyuki <katsuyuki.russell@gmail.com>
2023-07-27 15:35:05 -0500

    added api fetch for location input

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 //Variables
-
-// function for city fetch
-
-    // apply data from fetch
-
-    // 
+var input = document.getElementById("myInput")
+var button = document.getElementById("myButton")
+var fillableUl = document.getElementById("fillThisUl")
+var weatherDisplay = document.getElementById("weatherdisplay")
+var localArray = []
+var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='
+// function for city location fetch
+function cityConverter(lat, lon) {
+    var apiUrl1 = weatherUrl + lat + '&lon=' + lon + '&units=imperial&appid=0f0f1cfbe3b125e774d9bc3e67107d8f'
+    fetch(apiUrl1)
+        .then(function(response) {
+            return response.json()})
+        .then(function(data){
+            while(weatherDisplay.firstChild){
+                weatherDisplay.removeChild(weatherDisplay.firstChild)
+            }
+            // apply data from fetch
+            for(i=0;i<5; i++){
+                
+            }
+        })
+    }    
